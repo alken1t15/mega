af78d3cac9fee665d36f4d96685633b38a3c6244
Alexey Zarubin <alken1t15@gmail.com>
2023-03-03 09:30:20 +0600

    first push

---
+++ back/back-for_mega/src/main/java/kz/runtime/backfor_mega/BackForMegaApplication.java
@@ -0,0 +1,13 @@
+package kz.runtime.backfor_mega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackForMegaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackForMegaApplication.class, args);
+    }
+
+}
---
+++ back/back-for_mega/src/test/java/kz/runtime/backfor_mega/BackForMegaApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.runtime.backfor_mega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackForMegaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
